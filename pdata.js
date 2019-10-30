c060f70ee777986f3b0974b3a9d0fdff542ad64c
XiaoGeNintendo <34835642+XiaoGeNintendo@users.noreply.github.com>
2019-10-30 22:41:26 +0800

    pset

---
+++ pdata.js
@@ -0,0 +1,18 @@
+var problems=[
+    {
+        name:"A+B Problem",
+        statement:"You are given two integers A and B. Add them.",
+        input:"var A=...;<br/>var B=...;",
+        output:"Answer should be stored in C",
+        test:[
+            {
+                input:["var a=1;","var b=1;"],
+                check:"c==2"
+            },
+            {
+                input:["var a=2;","var b=3;"],
+                check:"c==5"
+            }
+        ]
+    }
+]
